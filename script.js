c7b895c6bb42127823d9926373becf93473979db
sylvia <ojitoscecy.com>
2020-04-12 21:29:56 -0500

    adding Todo-Filtering

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const fetchTodos = () => {
 const logTodos = () => {
     console.log(arrayOfTodos)
 }
-
+//original
 const populateTodos =() => {
     var todoList = document.getElementById("todo-list");
     for (i=0; i < arrayOfTodos.length; i++) {
@@ -28,6 +28,13 @@ const populateTodos =() => {
         var itemText = document.createTextNode(arrayOfTodos[i].title);
         todoListItem.appendChild(itemText);
         todoList.appendChild(todoListItem);
+        if (!arrayOfTodos[i].completed){
+            todoListItem.style.setProperty('color', 'red');
+        }else{
+            todoListItem.style.setProperty('color', 'green');
+        }
+        todoList.appendChild(todoListItem);
+        
     }
 }
 const arrayOfTodos1 = [
@@ -47,3 +54,16 @@ const arrayOfTodos1 = [
 
 console.log(arrayOfTodos1[0].userId)//=> 14
 console.log(arrayOfTodos1[1].userId)//=> 20
+
+//
+const list = document.getElementById('todo-list')
+let redTodos = []
+let redCompleteTodos = []
+
+var idInput = 0
+var lastIndex = 0
+var indexLimit = 20
+var isPopulated = false
+var isFiltered = false
+var isCompleted = null
+var correctInput = false
